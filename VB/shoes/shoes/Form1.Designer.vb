ff86f65e2a5ded66cf74672dae26188889f3d558
nicole <niwal889@learner.dvusd.org>
2022-08-17 08:26:22 -0700

    wedensday

---
+++ VB/shoes/shoes/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.jordanButton = New System.Windows.Forms.Button()
+        Me.displayPictureBox = New System.Windows.Forms.PictureBox()
+        Me.jordanPictureBox = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.pricelabel = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.dunksButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.convereseButton = New System.Windows.Forms.Button()
+        Me.converese = New System.Windows.Forms.PictureBox()
+        Me.vansbutton = New System.Windows.Forms.Button()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.blazersbutton = New System.Windows.Forms.Button()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.jordanPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.converese, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'jordanButton
+        '
+        Me.jordanButton.Location = New System.Drawing.Point(21, 275)
+        Me.jordanButton.Name = "jordanButton"
+        Me.jordanButton.Size = New System.Drawing.Size(75, 23)
+        Me.jordanButton.TabIndex = 0
+        Me.jordanButton.Text = "jordans"
+        Me.jordanButton.UseVisualStyleBackColor = True
+        '
+        'displayPictureBox
+        '
+        Me.displayPictureBox.Location = New System.Drawing.Point(-1, -2)
+        Me.displayPictureBox.Name = "displayPictureBox"
+        Me.displayPictureBox.Size = New System.Drawing.Size(324, 178)
+        Me.displayPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.displayPictureBox.TabIndex = 1
+        Me.displayPictureBox.TabStop = False
+        '
+        'jordanPictureBox
+        '
+        Me.jordanPictureBox.Image = CType(resources.GetObject("jordanPictureBox.Image"), System.Drawing.Image)
+        Me.jordanPictureBox.Location = New System.Drawing.Point(21, 303)
+        Me.jordanPictureBox.Name = "jordanPictureBox"
+        Me.jordanPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.jordanPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.jordanPictureBox.TabIndex = 2
+        Me.jordanPictureBox.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(367, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "manufactored"
+        '
+        'pricelabel
+        '
+        Me.pricelabel.AutoSize = True
+        Me.pricelabel.Location = New System.Drawing.Point(367, 67)
+        Me.pricelabel.Name = "pricelabel"
+        Me.pricelabel.Size = New System.Drawing.Size(37, 16)
+        Me.pricelabel.TabIndex = 4
+        Me.pricelabel.Text = "price"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(367, 110)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "first made"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(367, 160)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(162, 16)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "who came up with the idea"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(462, 23)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 16)
+        Me.Label5.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(419, 67)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 16)
+        Me.Label6.TabIndex = 9
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(447, 110)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(0, 16)
+        Me.Label7.TabIndex = 10
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(544, 160)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 16)
+        Me.Label8.TabIndex = 11
+        '
+        'dunksButton
+        '
+        Me.dunksButton.Location = New System.Drawing.Point(141, 274)
+        Me.dunksButton.Name = "dunksButton"
+        Me.dunksButton.Size = New System.Drawing.Size(75, 23)
+        Me.dunksButton.TabIndex = 12
+        Me.dunksButton.Text = "dunks"
+        Me.dunksButton.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(141, 302)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 13
+        Me.PictureBox1.TabStop = False
+        '
+        'convereseButton
+        '
+        Me.convereseButton.Location = New System.Drawing.Point(260, 274)
+        Me.convereseButton.Name = "convereseButton"
+        Me.convereseButton.Size = New System.Drawing.Size(128, 22)
+        Me.convereseButton.TabIndex = 14
+        Me.convereseButton.Text = "converses"
+        Me.convereseButton.UseVisualStyleBackColor = True
+        '
+        'converese
+        '
+        Me.converese.Image = CType(resources.GetObject("converese.Image"), System.Drawing.Image)
+        Me.converese.Location = New System.Drawing.Point(260, 302)
+        Me.converese.Name = "converese"
+        Me.converese.Size = New System.Drawing.Size(100, 50)
+        Me.converese.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.converese.TabIndex = 15
+        Me.converese.TabStop = False
+        '
+        'vansbutton
+        '
+        Me.vansbutton.Location = New System.Drawing.Point(410, 275)
+        Me.vansbutton.Name = "vansbutton"
+        Me.vansbutton.Size = New System.Drawing.Size(75, 23)
+        Me.vansbutton.TabIndex = 16
+        Me.vansbutton.Text = "vans"
+        Me.vansbutton.UseVisualStyleBackColor = True
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(400, 304)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 17
+        Me.PictureBox3.TabStop = False
+        '
+        'blazersbutton
+        '
+        Me.blazersbutton.Location = New System.Drawing.Point(547, 275)
+        Me.blazersbutton.Name = "blazersbutton"
+        Me.blazersbutton.Size = New System.Drawing.Size(75, 23)
+        Me.blazersbutton.TabIndex = 18
+        Me.blazersbutton.Text = "blazers"
+        Me.blazersbutton.UseVisualStyleBackColor = True
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(541, 304)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 19
+        Me.PictureBox4.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.blazersbutton)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.vansbutton)
+        Me.Controls.Add(Me.converese)
+        Me.Controls.Add(Me.convereseButton)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.dunksButton)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.pricelabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.jordanPictureBox)
+        Me.Controls.Add(Me.displayPictureBox)
+        Me.Controls.Add(Me.jordanButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.jordanPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.converese, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents jordanButton As Button
+    Friend WithEvents displayPictureBox As PictureBox
+    Friend WithEvents jordanPictureBox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents pricelabel As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents dunksButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents convereseButton As Button
+    Friend WithEvents converese As PictureBox
+    Friend WithEvents vansbutton As Button
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents blazersbutton As Button
+    Friend WithEvents PictureBox4 As PictureBox
+End Class
---
+++ VB/shoes/shoes/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub jordanButton_Click(sender As Object, e As EventArgs) Handles jordanButton.Click
+        displayPictureBox.Image = jordanPictureBox.Image
+        Label5.Text = "nike"
+        Label6.Text = "average is $145"
+        Label7.Text = "1985"
+        Label8.Text = "Michael Jordan"
+    End Sub
+
+    Private Sub convereseButton_Click(sender As Object, e As EventArgs) Handles convereseButton.Click
+        displayPictureBox.Image = converese.Image
+        Label5.Text = "converese"
+        Label6.Text = "$40-$70"
+        Label7.Text = "1908"
+        Label8.Text = "Marqius Mills Converse"
+    End Sub
+
+    Private Sub vansbutton_Click(sender As Object, e As EventArgs) Handles vansbutton.Click
+        displayPictureBox.Image = PictureBox3.Image
+
+    End Sub
+
+    Private Sub jordan1Button_Click(sender As Object, e As EventArgs) Handles jordanButton.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+
+    Private Sub jordanPictureBox_Click(sender As Object, e As EventArgs) Handles jordanPictureBox.Click
+
+    End Sub
+
+    Private Sub dunksButton_Click(sender As Object, e As EventArgs) Handles dunksButton.Click
+        displayPictureBox.Image = PictureBox1.Image
+        Label5.Text = "nike"
+        Label6.Text = "around $100"
+        Label7.Text = "1985"
+        Label8.Text = "Peter Moore"
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub pricelabel_Click(sender As Object, e As EventArgs) Handles pricelabel.Click
+
+    End Sub
+End Class
+
